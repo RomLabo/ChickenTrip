3ad071a1aa34dcbe508c58eca5dfca1f87d80d2b
RomL <rom.laborde@orange.fr>
2022-02-13 21:32:30 +0100

    Test texture

--- js/App.js
+++ js/App.js
@@ -5,7 +5,7 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const image = new Image();
-image.src = './textures/wood.png';
+image.src = './textures/wood01.png';
 
 let coordinateX = 0;
 console.log(10 % canvas.width + canvas.width)
@@ -14,8 +14,8 @@ const render = () => {
     coordinateX ++;
     
     // Background
-    context.drawImage(image, 0, 0, 768, 432, -(coordinateX % canvas.width) + canvas.width, 0, canvas.width, canvas.height);
     context.drawImage(image, 0, 0, 768, 432, -(coordinateX % canvas.width), 0, canvas.width, canvas.height);
+    context.drawImage(image, 0, 0, 768, 432, -(coordinateX % canvas.width) + canvas.width, 0, canvas.width, canvas.height);
     
     // Chicken
     context.drawImage(image, 0, 496, 65, 65, canvas.width * .04, canvas.height * .8, canvas.width * .07, canvas.width * .07)
