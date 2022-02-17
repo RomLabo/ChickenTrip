c81a5031517ffd592e94859b439b4045dacccea8
RomL <rom.laborde@orange.fr>
2022-02-17 22:35:32 +0100

    Modif accélération GameButcher

--- js/templates/GameButcher.js
+++ js/templates/GameButcher.js
@@ -4,24 +4,37 @@ class GameButcher {
         this.context = this.setting.context;
         this.canvasSize = this.setting.canvasSize;
 
+        this.butcherVariablePositionX = this.canvasSize[0];
         this.butcherSize = [154, 180];
         this.butcherResponsiveSize = [this.canvasSize[0] * .15, this.canvasSize[0] * .18];
-        this.butcherPosition = [this.butcherVariablePositionX, this.canvasSize[1] * .55];
+        this.butcherPosition = [0, this.canvasSize[1] * .55];
+        this.variablePosY = this.butcherPosition[1];
 
-        this.butcherVariablePositionX;
+        this.difficulty = this.setting.difficultyLevel;
+        
     }
     createButcher() {
         let butcherMotion = Math.floor((this.setting.gameIndex % 18) / 6) * this.butcherSize[0];
-        let butcherMotionX = (this.setting.gameIndex * this.setting.difficultyLevel) % this.canvasSize[0];
+        let butcherMotionX = (this.setting.gameIndex * this.difficulty) % this.canvasSize[0];
         let butcherTextureParams = [this.setting.texture, butcherMotion, 617, ...this.butcherSize];
+        
         let butcherCanvasFirstParams = [- butcherMotionX, this.butcherPosition[1], ...this.butcherResponsiveSize];
         let butcherCanvasSecondParams = [- butcherMotionX + this.canvasSize[0], this.butcherPosition[1], ...this.butcherResponsiveSize];
-        let mathX = (Math.floor(Math.random() * (100 - 1 + 1)) + 1) % 10;
-        //console.log(butcherMotionX)
-        console.log(Math.floor(- butcherMotionX + this.canvasSize[0]));
-
+        let variablePos = Math.floor(this.setting.gameIndex % (this.canvasSize[0] * 2));
+        
+        //console.log(variablePos)
+        switch (true) {
+            case variablePos === 870:
+                this.difficulty += 2;
+                break;
+            case variablePos === 1125:
+                this.difficulty -= 2;
+                break;
+            default:
+                console.log(this.difficulty)
+        }
         this.context.drawImage(...butcherTextureParams, ...butcherCanvasFirstParams);
-        this.context.drawImage(...butcherTextureParams, ...butcherCanvasSecondParams); 
+        this.context.drawImage(...butcherTextureParams, ...butcherCanvasSecondParams);
     }
     renderButcher() {
         this.createButcher();
