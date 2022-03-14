c2f4a8eb7273d2f3c033c04e1d0dafb41fa46250
RomL <rom.laborde@orange.fr>
2022-03-14 22:15:41 +0100

    Ajout d'un 2ème ennemi

--- js/models/SetupEnemies.js
+++ js/models/SetupEnemies.js
@@ -2,8 +2,10 @@ class SetupEnemies {
     constructor(setup) {
         this._setup = setup;
         this._size = [122, 180];
+        this._secondeSize = [180, 152];
         this._responsiveSize = [setup.canvasSize[0] * .12 | 0, setup.canvasSize[0] * .15 | 0];
         this._positionY = setup.canvasSize[1] * .9 - this._responsiveSize[1] | 0;
+        this._secondePositionY = setup.canvasSize[1] * .3 - this._responsiveSize[1] | 0;
         this._speedRun = setup.difficultyLevel * 2;
         this._variableGap = 2;
     }
@@ -12,7 +14,9 @@ class SetupEnemies {
     }
     get textureParams() {
         let motion = Math.floor((this._setup._gameIndex % 15) / 5) * this._size[0];
-        return [this._setup.texture, motion, 617, ...this._size];
+        let secondeMotion = Math.floor((this._setup._gameIndex % 18) / 9) * this._secondeSize[0];
+        return [[this._setup.texture, motion, 617, ...this._size],
+        [this._setup.texture, secondeMotion + 400, 617, ...this._secondeSize]];
     }
     get positionY() {
         return this._positionY;
@@ -20,6 +24,7 @@ class SetupEnemies {
     get canvasParams() {
         let interval = Math.floor(this._setup.canvasSize[0] / this._speedRun) * this._speedRun;
         let variableX = (this._setup._gameIndex * this._speedRun) % (this._variableGap * interval);
-        return [-variableX + interval, this._positionY, ...this._responsiveSize];
+        return [[-variableX + interval, this._positionY, ...this._responsiveSize],
+        [-variableX + interval + 200, this._secondePositionY, ...this._responsiveSize]];
     }
 }
--- js/templates/GameEnemies.js
+++ js/templates/GameEnemies.js
@@ -5,16 +5,17 @@ class GameEnemies {
         this.scorePoints = 0;
     }
     drawCharacter() {
-        this.setup.context.drawImage(...this.setup.textureParams, ...this.setup.canvasParams);
+        this.setup.context.drawImage(...this.setup.textureParams[0], ...this.setup.canvasParams[0]);
+        this.setup.context.drawImage(...this.setup.textureParams[1], ...this.setup.canvasParams[1]);
     }
     detectCrash(chickenPosition) {
-        let crashParamsX = [this.setup.canvasParams[0], this.setup.canvasParams[0] + (this.setup.canvasParams[3] * .5)];
+        let crashParamsX = [this.setup.canvasParams[0][0], this.setup.canvasParams[0][0] + (this.setup.canvasParams[0][3] * .5)];
         if (crashParamsX[0] <= chickenPosition[1] 
             && crashParamsX[1] >= chickenPosition[0] 
             && chickenPosition[2] >= this.setup.positionY) {
             this.isCrash = true;
         } 
-        if (this.setup.canvasParams[0] === 0) {
+        if (this.setup.canvasParams[0][0] === 0) {
             this.scorePoints ++;
         }
     }
